1c7268b31c18e393da6a1395de9062ddf0dc8791
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-10-24 23:53:15 -0400

    rewrote recieveFlight and pricedItinerary functions to log the flight segment and base fare fee.

--- assets/js/second.js
+++ assets/js/second.js
@@ -54,50 +54,51 @@ var inputLocation = function (desiredLocation) {
   
   fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/locations?name=' + desiredLocation, options)
     .then(response => response.json())
-    .then(response => console.log(response[0].cityName))
+    .then(response => recieveFlight(response[0].cityCode))
     .catch(err => console.error(err));
+};
+
+
+// places city code into priceline and search flights
+var recieveFlight = function (cityCode) {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '89c9b663dbmshd869209e6d40f5ep11340fjsnebd044f6c87a',
+      'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/search?itinerary_type=ONE_WAY&class_type=ECO&location_arrival=' + cityCode  + '&date_departure=2022-11-15&location_departure=TPA&sort_order=PRICE&price_max=20000&number_of_passengers=1&duration_max=2051&price_min=100&date_departure_return=2022-11-16', options)
+    .then(response => response.json())
+    .then(response => pricedItinerary(response.segment[1], cityCode))
+    .catch(err => console.error(err));
+};
+
+// passes flight segments and city code to grab price itinerary
+var pricedItinerary = function (flights, cityCode){
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '89c9b663dbmshd869209e6d40f5ep11340fjsnebd044f6c87a',
+      'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/search?itinerary_type=ONE_WAY&class_type=ECO&location_arrival=' + cityCode + '&date_departure=2022-11-15&location_departure=TPA&sort_order=PRICE&price_max=20000&number_of_passengers=1&duration_max=2051&price_min=100&date_departure_return=2022-11-16', options)
+    .then(response => response.json())
+    .then(response => console.log(response.pricedItinerary[0].pricingInfo['baseFare'], flights, cityCode))
+    .catch(err => console.error(err));
+
 };
 
 // made it to here tonight !!!!!!!!!!!!!!!!!
 
-// places city code into priceline and search flights
-var recieveFlight = function (cityCode) {
-  let origin = localStorage.getItem("departureLocation")
-  const options = {
-	method: 'GET',
-	headers: {
-	  'X-RapidAPI-Key': '     89c9b663dbmshd869209e6d40f5ep11340fjsnebd044f6c87a',
-	  'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
-	}
-  };
-	
-	fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/search?itinerary_type=ONE_WAY&class_type=ECO&location_arrival=' + cityCode + '&date_departure=2022-11-15&location_departure=' + origin + '&sort_order=PRICE&price_max=20000&number_of_passengers=1&duration_max=2051&price_min=100&date_departure_return=2022-11-16', options)
-	  .then(response => response.json())
-	  .then(response => pricedItinerary(response.segment, cityCode))
-	  .catch(err => console.error(err));
-};
-
-// place city and flights info and grab price itinerary for all flights
-var pricedItinerary = function (flights, city){
-  const options = {
-	method: 'GET',
-	headers: {
-	  'X-RapidAPI-Key': '     89c9b663dbmshd869209e6d40f5ep11340fjsnebd044f6c87a',
-	  'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
-	}
-  };
-	
-	fetch('https://priceline-com-provider.p.rapidapi.com/v1/flights/search?itinerary_type=ONE_WAY&class_type=ECO&location_arrival=' + city + '&date_departure=2022-11-15&location_departure=TPA&sort_order=PRICE&price_max=20000&number_of_passengers=1&duration_max=2051&price_min=100&date_departure_return=2022-11-16', options)
-	  .then(response => response.json())
-	  .then(response => {
-      // console.log(response.pricedItinerary, flights, city);
-      displayFlights(response.pricedItinerary, flights, city)})
-	  .catch(err => console.error(err));
-
-};
 
 // create a function that will loop through all flights info and itinerary pricing and display on page
-var displayFlights = function (prices, flights, city) {
+var displayFlights = function (prices, flights, cityCode) {
   // console.log("Flights:", flights);
   if (flights.length === 0) {
     searchedFlightsEl.textContent = 'No flights found.';
